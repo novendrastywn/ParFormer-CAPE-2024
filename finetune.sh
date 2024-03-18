4e1f9c5f424d4f81eedcce17822bb677689910a2
novendrasetyawan <31612686+novendrasetyawan@users.noreply.github.com>
2024-03-18 14:42:43 +0800

    finetune.sh

--- finetune.sh
+++ finetune.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 export NCCL_LL_THRESHOLD=0
-dataset_path='/home/ndr/Documents/ImageDataset/ImageNet-1K'
+dataset_path='dataset_path'
 
 PORT=${PORT:-888z} GPUS=1 batch_size=128 epochs=30
 dataset=IMNET
 
-MODEL=ParFormer_S  OUTPUT_DIR=./checkpoints/finetune/$MODEL
+MODEL=ParFormer_B1  OUTPUT_DIR=./checkpoints/finetune/$MODEL
 python -m torch.distributed.launch --nproc_per_node=$GPUS main.py \
 --model $MODEL --drop_path 0.1 \
 --data_set $dataset --data_path=$dataset_path --print_freq=10 \
@@ -13,8 +13,8 @@ python -m torch.distributed.launch --nproc_per_node=$GPUS main.py \
 --opt lamb --lr 1e-4 --warmup_epochs=0 --update_freq 4 \
 --head_init_scale 0.001 --cutmix 0 --mixup 0 \
 --enable_wandb true --project=ParFormer_Finetune \
---finetune /home/ndr/Documents/VisionWorkspace/checkpoints/$MODEL/checkpoint-best.pth \
---input_size 224 \
+--finetune ${checkpoint/Model.pth}\
+--input_size 384 \
 --output_dir $OUTPUT_DIR
 
 
