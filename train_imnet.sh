6ad678b49744c6defce276058cb21736760539c1
novendrastywn <31612686+novendrastywn@users.noreply.github.com>
2024-03-28 13:48:46 +0800

    train_imnet.sh

--- train_imnet.sh
+++ train_imnet.sh
@@ -1,12 +1,15 @@
 export NCCL_LL_THRESHOLD=0
 dataset_path='dataset_path'
 
-PORT=${PORT:-8888} GPUS=1 batch_size=128 epochs=300
+model=$1
+gpus=$2
+
+PORT=${PORT:-8888} batch_size=128 epochs=300
 dataset=IMNET
 
-MODEL=ParFormer_B1 OUTPUT_DIR=./checkpoints/$MODEL
-python -m torch.distributed.launch --nproc_per_node=$GPUS main.py \
---model $MODEL --drop_path 0.0001 --model_ema true --model_ema_eval true --auto_resume=true \
+OUTPUT_DIR=./checkpoints/$MODEL
+python -m torch.distributed.launch --nproc_per_node=$gpus main.py \
+--model $model --drop_path 0.0001 --model_ema true --model_ema_eval true --auto_resume=true \
 --data_set $dataset --data_path=$dataset_path --update_freq=4 \
 --batch_size $batch_size --epochs=$epochs --num_workers=10 \
 --opt adamW --lr 4e-3 --warmup_epochs=20 --input_size 224 \
